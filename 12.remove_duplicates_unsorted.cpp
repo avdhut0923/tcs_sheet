483b016940bdee0ac0e1dc0f0cba0f6a01517f70
avdhut0923 <122094288+avdhut0923@users.noreply.github.com>
2024-12-21 12:05:18 +0530

    Create 12.remove_duplicates_unsorted.cpp

---
+++ 12.remove_duplicates_unsorted.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void usortRemDup(int arr[],int n){
+    int mark[n];
+    for(int i=0;i<n;i++){
+         mark[i] = 1;
+    }
+        // all elements in an array are false 
+    for(int i=0;i<n;i++){
+        if (mark[i]==1){
+            for(int j=i+1;j<n;j++){
+                if(arr[i]==arr[j]){
+                    mark[j]=0;
+                }
+            }
+        }
+
+    }
+    for(int i=0;i<n;i++){
+        if(mark[i]==1){
+            cout<<arr[i]<<" ";
+        }
+    }
+
+}
+
+int main(){
+    int arr[] = {2,3,54,2,5,6,7,3};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    usortRemDup(arr,n);
+}
