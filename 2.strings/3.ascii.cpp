7ba8cea9dd0bbb402e0ea3cca3a94b5c161fcd05
avdhut0923 <122094288+avdhut0923@users.noreply.github.com>
2025-01-03 08:22:38 +0530

    Add files via upload

---
+++ 2.strings/3.ascii.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+ int ascii(char a){
+    int value = a;
+    return value;
+
+ }
+
+int main(){
+    char var ;
+    cout << "enter variable"<<endl;
+    cin>>var;
+    cout<<"ascii value of "<<var<<" is "<< ascii(var);
+}
