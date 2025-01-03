1474d6f819929e367e3b5e46311f34cec7593293
avdhut0923 <122094288+avdhut0923@users.noreply.github.com>
2025-01-03 12:34:03 +0530

    Create 3.cummulative_freq.cpp

---
+++ gfg_1.array/3.cummulative_freq.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    vector<int> countFreq(vector<int>& arr) {
+        map<int,int>mp;
+        int n=arr.size();
+        vector<int>ans;
+        for(int i=0;i<n;i++){
+            mp[arr[i]]++;
+        }
+        int cum=0;
+        for(auto it:mp){
+          cum=cum+it.second;
+          ans.push_back(cum);
+        }
+        return ans;
+        
+    }
+};
