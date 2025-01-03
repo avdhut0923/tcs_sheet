7aab086b4845819126d7787a2c60b0ae25544844
avdhut0923 <122094288+avdhut0923@users.noreply.github.com>
2025-01-03 13:10:43 +0530

    Add files via upload

---
+++ 2.strings/7.3inbuilt.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+     string str = "Jai Jai Ram";
+     reverse(str.begin(),str.end());
+     cout<<"the reversed string is ";
+     cout<<str;
+}
