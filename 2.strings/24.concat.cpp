c8e794e59a85092e12c6caef844af0bc6c713d57
avdhut0923 <122094288+avdhut0923@users.noreply.github.com>
2025-01-11 14:36:00 +0530

    Add files via upload

---
+++ 2.strings/24.concat.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string str1="ram";
+    string str2="krishna";
+    string str3="Hari";
+
+    string ans = str1+str2+str3;
+    cout<<ans;
+    return 0;
+}
