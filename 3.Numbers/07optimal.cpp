a1e98691b0d6a5fffa6317bb2e4ae9ff076f86be
avdhut0923 <122094288+avdhut0923@users.noreply.github.com>
2025-01-15 13:08:01 +0530

    bitwise and

---
+++ 3.Numbers/07optimal.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter no.";
+    cin>>n;
+    if(n&1==0)cout<<"even no. hai";  // bitwise operations are faster than normal operations 
+    else cout<<"odd no.";
+}
