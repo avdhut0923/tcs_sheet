fdb589d12e25cfe98de77578b36613c0e0a89602
avdhut0923 <122094288+avdhut0923@users.noreply.github.com>
2025-01-15 13:55:55 +0530

    Add files via upload

---
+++ 3.Numbers/09sumOFn.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter no.";
+    cin>>n;
+    cout<<"sum of first n natural no.s is "<< endl;
+    cout<<n*(n+1)/2;
+}
