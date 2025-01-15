d709a187f68fdbf3b53f287a0940859e39fba9f7
avdhut0923 <122094288+avdhut0923@users.noreply.github.com>
2025-01-15 13:17:34 +0530

    Add files via upload

---
+++ 3.Numbers/08posORnegtive.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter no.";
+    cin>>n;
+    if(n>0)cout<<"positive";
+    else if(n<0)cout<<"negative";
+    else cout<<"zero";
+}
