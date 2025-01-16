36e030499d2f4d50dadae0814f10a6bd6dbb1b46
avdhut0923 <122094288+avdhut0923@users.noreply.github.com>
2025-01-16 08:40:07 +0530

    Add files via upload

---
+++ 3.Numbers/10formula.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n=5;
+    int d=-3;
+    int a = -3;
+    float sum=0;
+    sum=n/2.0*(2*a + (n-1)*d);
+    cout<< sum;
+    
+}
