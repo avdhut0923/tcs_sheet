d8114c8477eb1d1eae1d2983527a43f5b8362a9d
avdhut0923 <122094288+avdhut0923@users.noreply.github.com>
2025-01-19 10:05:16 +0530

    class

---
+++ 3.Numbers/34area_circle.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Area {
+    public:
+    void areaOfCircle(int n){
+        double ans = n*n*3.14;
+        cout<<"area of circle : ";
+        cout<<ans;
+    }
+};
+
+int main(){
+    int r = 5;
+    Area s1;
+    s1.areaOfCircle(r);
+    
+       
+}
